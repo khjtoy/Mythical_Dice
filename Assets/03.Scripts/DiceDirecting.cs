1bf19084fd1f74439e8bab706c258df55a85f3ad
kdh123456 <77655332+kdh123456@users.noreply.github.com>
2022-07-11 11:39:51 +0900

    다이스 조금 수정할뻔

--- Assets/03.Scripts/DiceDirecting.cs
+++ Assets/03.Scripts/DiceDirecting.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class DiceDirecting : MonoBehaviour
 {
@@ -27,8 +28,10 @@ public class DiceDirecting : MonoBehaviour
 		}
 		else if (Input.GetMouseButtonDown(0) && isDiceDirecting)
 		{
+			int Randoms = Random.Range(1, 6);
+			DiceObjet.transform.localRotation = Quaternion.Euler(DiceRotationVector[Randoms]);
 			isDiceDirecting = false;
-			for(int i = 0; i<diceParticel.Length; i++)
+			for (int i = 0; i < diceParticel.Length; i++)
 			{
 				diceParticel[i].Clear();
 			}
@@ -36,8 +39,6 @@ public class DiceDirecting : MonoBehaviour
 			{
 				diceParticel[i].Play();
 			}
-			int Randoms = Random.Range(1, 6);
-			DiceObjet.transform.localRotation = Quaternion.Euler(DiceRotationVector[Randoms]);
 		}
 	}
 }
