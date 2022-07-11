4cb8aba2b7d19ab30953e703df33dee696e1f872
DESKTOP-OHD6DA9\USER <khjgogle@gmail.com>
2022-07-11 17:06:33 +0900

    병합1

--- Assets/03.Scripts/Map/MapController.cs
+++ Assets/03.Scripts/Map/MapController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class MapController : MonoBehaviour
 {
-<<<<<<< HEAD
     [SerializeField]
     private GameObject dicePrefabs;
     [SerializeField]
@@ -21,6 +20,10 @@ public class MapController : MonoBehaviour
 
     private GameManager gameManager;
 
+	public bool isDual;
+	public bool XAxis;
+	public bool isDown;
+	public bool isLeft;
 
     private void Awake()
     {
@@ -50,43 +53,6 @@ public class MapController : MonoBehaviour
             }
         }
     }
-=======
-	[SerializeField]
-	private GameObject dicePrefabs;
-	[SerializeField]
-	private Transform root;
-
-	[Header("크기 지정 변수")]
-	[SerializeField]
-	private int width;
-	[SerializeField]
-	private int height;
-
-	[SerializeField]
-	private float distance;
-
-	[SerializeField]
-	private int size;
-
-	private Vector2 min;
-
-	private GameObject[][] map;
-
-	public bool isDual;
-	public bool XAxis;
-	public bool isDown;
-	public bool isLeft;
-
-	private void Awake()
-	{
-		min = new Vector2(size / 2, size / 2) * -1.5f;
-		map = new GameObject[height][];
-	}
-	private void Start()
-	{
-		if (root.childCount == 1)
-			SpawnMap();
-	}
 
 	private void Update()
 	{
@@ -96,34 +62,18 @@ public class MapController : MonoBehaviour
 		}
 	}
 
-	private void SpawnMap()
-	{
-		for (int y = 0; y < height; y++)
-		{
-			map[y] = new GameObject[width];
-			for (int x = 0; x < width; x++)
-			{
-				map[y][x] = Instantiate(dicePrefabs, new Vector3(0, 0, 0), Quaternion.identity);
-				map[y][x].transform.SetParent(root);
-				map[y][x].transform.localPosition = new Vector3(min.x + (1.5f * x), min.y + (1.5f * y), 0);
-				map[y][x].transform.localRotation = Quaternion.Euler(180, 0, 0);
-				map[y][x].transform.localScale = new Vector3(1, 1, 1);
-			}
-		}
-	}
-
 	private void FloorDirect(int x = 0, int y = 0, bool isfirst = false)
 	{
 		if (isDown && !isfirst)
 		{
-			y = height - 1;
+			y = GameManager.Instance.Height - 1;
 		}
 		if (isLeft && !isfirst)
 		{
-			x = width - 1;
+			x = GameManager.Instance.Width - 1;
 		}
 
-		if (y < 0 || y >= height || x < 0 || x >= width && isDual)
+		if (y < 0 || y >= GameManager.Instance.Height || x < 0 || x >= GameManager.Instance.Width && isDual)
 		{
 			return;
 		}
@@ -135,10 +85,10 @@ public class MapController : MonoBehaviour
 			x = y < 0 ? x + 1 : x;
 			y = y < 0 ? 0 : y;
 
-			y = x >= width ? y + 1 : y;
-			x = x >= width ? width - 1 : x;
-			x = y >= height ? x + 1 : x;
-			y = y >= height ? height - 1 : y;
+			y = x >= GameManager.Instance.Width ? y + 1 : y;
+			x = x >= GameManager.Instance.Width ? GameManager.Instance.Width - 1 : x;
+			x = y >= GameManager.Instance.Height ? x + 1 : x;
+			y = y >= GameManager.Instance.Height ? GameManager.Instance.Height - 1 : y;
 		}
 
 		map[y][x].transform.GetChild(2).localRotation = Quaternion.Euler(0,0,0);
@@ -176,5 +126,4 @@ public class MapController : MonoBehaviour
 				FloorDirect(x, y + 1, isfirst);
 		}
 	}
->>>>>>> origin/kdh
 }
