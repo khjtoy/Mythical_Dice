9ae52d2d9a056f9fd5da0095fd12220600be22d9
csh4430 <snghun6889@gmail.com>
2022-07-13 19:19:24 +0900

    AI 사용가능

--- Assets/03.Scripts/Enemy/Mino/MinoStamp.cs
+++ Assets/03.Scripts/Enemy/Mino/MinoStamp.cs
@@ -16,5 +16,39 @@ public class MinoStamp : EnemyMove
         seq.Append(transform.DOLocalMoveZ(-3, 0.3f));
         seq.Append(transform.DOLocalMove(new Vector3(target.x, target.y, -3), 0.3f));
         seq.Append(transform.DOLocalMoveZ(-1, 0.1f).SetEase(Ease.InExpo));
+
+
+        
+        seq.AppendCallback(() =>
+        {
+            StartCoroutine(StapCoroutine());
+            seq.Kill();
+            IsFloating = false;
+        });
+    }
+
+    private IEnumerator StapCoroutine()
+    {
+        Vector2Int pos = new Vector2Int(MapController.PosToArray(transform.localPosition.x), MapController.PosToArray(transform.localPosition.y));
+        GameManager.Instance.BossNum = 1;
+        for (int i = 1; i <=  GameManager.Instance.Size; i++)
+        {
+            for (int j = -i; j <= i; j++)
+            {
+                for (int k = -i; k <= i; k++)
+                {
+                    if (pos.y + j < 0 || pos.y + j >= GameManager.Instance.Size || pos.x + k < 0 || pos.x + k >= GameManager.Instance.Size)
+                        continue;
+                    if(pos.y + j == pos.y + i || pos.y + j == pos.y - i
+                        || pos.x + k == pos.x + i || pos.x + k == pos.x - i)
+                    {
+                        MapController.Instance.dices[pos.y + j][pos.x + k].DiceNumSelect(1);
+                        BoomMap.Instance.Boom(pos.x + k, pos.y + j);
+                    }
+
+                }
+            }
+            yield return new WaitForSeconds(0.5f);
+        }
     }
 }
