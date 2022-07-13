c1d392161871bb59be34e8f2218e4da11c4fd993
DESKTOP-OHD6DA9\USER <khjgogle@gmail.com>
2022-07-14 00:05:01 +0900

    Panel 추가

---
+++ Assets/03.Scripts/TutorialAction.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialAction : MonoBehaviour
+{
+   private bool isStart = false;
+
+    private void Start()
+    {
+        
+    }
+}
