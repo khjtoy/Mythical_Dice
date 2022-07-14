2075105397dbf16233e6ef64772c0d6c65877d96
ehddbs <ehddbs295@gmail.com>
2022-07-14 21:34:52 +0900

    Stage

--- Assets/03.Scripts/UI/Stage/StageManager.cs
+++ Assets/03.Scripts/UI/Stage/StageManager.cs
@@ -15,38 +15,51 @@ public class StageManager : MonoBehaviour
     [SerializeField]
     private RectTransform _fadePanel;
     private int currentStage;
+    private int OpenStage;
 
     private void Awake()
     {
         Time.timeScale = 1;
-        PlayerPrefs.SetInt("CLEAR", 0);
+        //PlayerPrefs.SetInt("CLEAR", 0);
+        //PlayerPrefs.SetInt("OPEN", 0);
+        OpenStage = PlayerPrefs.GetInt("OPEN");
         currentStage = PlayerPrefs.GetInt("CLEAR");
     }
 
     private void Start()
     {
+        for (int i = 0; i < OpenStage; i++)
+        {
+            stageList[i].SetActive(false);
+        }
         _fadePanel.anchoredPosition = new Vector3(0, 0, 0);
         Sequence seq = DOTween.Sequence();
         seq.AppendInterval(1);
         seq.Append(_fadePanel.DOAnchorPos3DY(1080, 1f));
         seq.AppendCallback(()=> {
-            seq.Kill();
             InitStage();
         });
     }
 
     private void InitStage()
     {
-        Debug.Log("asd");
+        
+        
         Sequence seq = DOTween.Sequence();
-        for (int i = 0;i<currentStage;i++)
+        for (int i = 0; i < currentStage; i++)
         {
-            Debug.Log(i);
-            seq.Append(stageList[i].transform.DOShakePosition(1, 10, 10));
-            int n = i;
-            seq.AppendCallback(() => stageList[n].SetActive(false));
+            if(stageList[i].activeSelf)
+            {
+                Debug.Log(i);
+                int n = i;
+                seq.Append(stageList[i].transform.DOShakePosition(1, 10, 10));
+                seq.AppendCallback(() => stageList[n].SetActive(false));
+            }
         }
-
+        if(OpenStage<currentStage)
+        {
+            PlayerPrefs.SetInt("OPEN", currentStage);
+        }
     }
 
     public void Stage(int stage)
@@ -57,5 +70,8 @@ public class StageManager : MonoBehaviour
         seq.Append(_fadePanel.DOAnchorPosY(0, 1f));
         seq.AppendCallback(() => SceneManager.LoadScene("GamePlay 6"));
     }
-
+    private void OnDisable()
+    {
+        DOTween.KillAll();
+    }
 }
