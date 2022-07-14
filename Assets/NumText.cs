dbcb611bc67ab2417564e0bf793ef337ac5f9cc9
kdh123456 <77655332+kdh123456@users.noreply.github.com>
2022-07-14 12:02:42 +0900

    버그픽스

--- Assets/NumText.cs
+++ Assets/NumText.cs
@@ -29,11 +29,11 @@ public class NumText : MonoBehaviour
 		num.alpha = 1;
 		Vector2 vec = Random.insideUnitCircle;
 		transform.position = new Vector3(Random.Range(pos.x - 1f, pos.x + 1f), Random.Range(pos.y, pos.y + 1f), pos.z);
-		transform.localEulerAngles = new Vector3(transform.rotation.x - 45, transform.rotation.y, transform.rotation.z);
+		//transform.localEulerAngles = new Vector3(transform.rotation.x - 45, transform.rotation.y, transform.rotation.z);
 		num.text = string.Format(text.ToString());
 		Sequence mySequence = DOTween.Sequence();
 		mySequence.Append(transform.DOMoveX(vec.x, HoriSpeed).SetEase(Ease.Linear));
-		mySequence.Join(transform.DOMoveY(Mathf.Abs(vec.y) + Power, PowerSpeed).SetEase(Ease.Linear)).AppendCallback(() => { num.DOFade(0, 0.35f);
+		mySequence.Join(transform.DOMoveY(Mathf.Abs(transform.position.y) + Power, PowerSpeed).SetEase(Ease.Linear)).AppendCallback(() => { num.DOFade(0, 0.35f);
 		});
 		mySequence.Append(transform.DOMoveY(Down, DownSpeed).SetEase(Ease.Linear)).AppendCallback(()=> { PoolManager.Instance.Despawn(this.gameObject); });
 	}
